2a5599b51b0746bb5cb8afa3c50f48971117a2b2
Vinícius Hoyer <vhoyer@live.com>
2019-06-23 09:06:11 -0300

    feat: add my account page with credits

---
+++ src/page/MyAccount.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class MyAccount extends React.Component {
+  render() {
+    return (
+      <div>
+        <footer>
+          Icons made by <a href="https://www.flaticon.com/authors/gregor-cresnar" title="Gregor Cresnar">Gregor Cresnar</a> and <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default MyAccount
