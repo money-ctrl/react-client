a2bc7ecb5ebf5069669a59f3b080694db7c2f89e
Vinícius Hoyer <vhoyer@live.com>
2019-06-22 21:22:33 -0300

    feat: add a dynamic cool background with animations

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Background from './layout/Background'
+import React from 'react'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Background />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/layout/Background.js
@@ -0,0 +1,84 @@
+import './Background.css'
+import React from 'react'
+
+class Background extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.moveItem = this.moveItem.bind(this)
+
+    this.state = {
+      items: Array.from({length:12}, () => ({
+        width: Math.random() * (150 - 80) + 80,
+        height: Math.random() * (150 - 80) + 80,
+        left: Math.random() * (window.innerWidth - 100),
+        top: Math.random() * (window.innerHeight - 100),
+        direction: Math.ceil(Math.random() * 4),
+        velocity: Math.ceil(Math.random() * 10),
+      })),
+    }
+  }
+
+  componentDidMount() {
+    this.interval = 100
+
+    this.updateTimerId = setInterval(() => {
+      this.setState(({items}) => ({
+        items: items.map(this.moveItem),
+      }))
+    }, this.interval)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.updateTimerId)
+  }
+
+  moveItem(item) {
+    const {left, top} = item
+
+    const newPosition = { left, top }
+    const velocity = item.velocity * (this.interval/1000)
+
+    switch(item.direction) {
+      case 1://left
+        newPosition.left += velocity
+        break
+      case 2://right
+        newPosition.left -= velocity
+        break
+      case 3://top
+        newPosition.top += velocity
+        break
+      default://bottom
+        newPosition.top -= velocity
+        break
+    }
+
+    if (newPosition.left + item.width < 0) newPosition.left = window.innerWidth
+    if (newPosition.left > window.innerWidth) newPosition.left = -item.width
+    if (newPosition.top + item.height < 0) newPosition.top = window.innerHeight
+    if (newPosition.top > window.innerHeight) newPosition.top = -item.height
+
+    return {
+      ...item,
+      ...newPosition,
+    }
+  }
+
+  render() {
+    const items = this.state.items.map(({top, left, height, width}) =>
+      <div
+        className="background__item"
+        style={{top, left, height, width}}
+      />
+    );
+
+    return (
+      <div className="background">
+        {items}
+      </div>
+    )
+  }
+}
+
+export default Background
