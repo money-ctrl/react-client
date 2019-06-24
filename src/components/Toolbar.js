b87aac8907577c7bfa283ecf81885ab0f3d0cbb5
Vinícius Hoyer <vhoyer@live.com>
2019-06-23 23:02:57 -0300

    feat: make scaffold for toolbar

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -4,17 +4,40 @@ import React from 'react'
 
 class Toolbar extends React.Component {
   render() {
+    const activeIndex = 0;
+
+    const menu = [{
+      alt: 'dashboard',
+      src: require('../assets/dashboard.svg'),
+    },{
+      alt: 'history',
+      src: require('../assets/feather.svg'),
+    },{
+      alt: 'account',
+      src: require('../assets/home.svg'),
+    }].map((item, index) =>
+      <img
+        src={item.src}
+        alt={item.alt}
+        height="30px"
+        width="30px"
+        className={activeIndex !== index ? 'toolbar__item--inactive' : ''}
+      />
+    )
+
     return (
       <Card className="toolbar">
-        <img
-          src={require('../assets/001-pie-chart.svg')}
-          alt=""
-          height="30px"
-          width="30px"
-        />
+        {menu}
 
-        <Card>
-          +
+        <Card
+          className="toolbar__action-button"
+        >
+          <img
+            src={require('../assets/plus.svg')}
+            alt="transaction"
+            height="30px"
+            width="30px"
+          />
         </Card>
       </Card>
     )
