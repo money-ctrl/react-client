edf571d197a353a7cdf8eb2a709cad1cdd7d7c73
Vinícius Hoyer <vhoyer@live.com>
2019-06-23 22:50:08 -0300

    feat: handpicked new icons

--- src/page/MyAccount.js
+++ src/page/MyAccount.js
@@ -5,7 +5,13 @@ class MyAccount extends React.Component {
     return (
       <div>
         <footer>
-          Icons made by <a href="https://www.flaticon.com/authors/gregor-cresnar" title="Gregor Cresnar">Gregor Cresnar</a> and <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a>
+          Icons made by
+          <a href="https://www.flaticon.com/authors/pixel-perfect" data-what="toolbar icons" title="Pixel perfect">Pixel perfect</a>
+          and
+          <a href="https://www.flaticon.com/authors/eucalyp" data-what="logo" title="Eucalyp">Eucalyp</a> from
+          <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+          is licensed by
+          <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a>
         </footer>
       </div>
     )
