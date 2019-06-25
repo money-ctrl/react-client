287e8b23bf3392536345ceacfe8c97039228f55e
Vinícius Hoyer <vhoyer@live.com>
2019-06-25 08:42:54 -0300

    Updates

---
+++ precache-manifest.2fefb24f1942ff340322853d7d248a2c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e91bed52fb8265f73730d23529c7934",
+    "url": "/react-client/index.html"
+  },
+  {
+    "revision": "770bdd836e17e81bcbaa",
+    "url": "/react-client/static/css/main.80f6b739.chunk.css"
+  },
+  {
+    "revision": "0afdea3d5674388b7870",
+    "url": "/react-client/static/js/2.3d00c51c.chunk.js"
+  },
+  {
+    "revision": "770bdd836e17e81bcbaa",
+    "url": "/react-client/static/js/main.d412abdf.chunk.js"
+  },
+  {
+    "revision": "4a24fb57e5e91233ecf3",
+    "url": "/react-client/static/js/runtime~main.1a624fa2.js"
+  },
+  {
+    "revision": "e78e43a1016c2d9307842acb71059c4b",
+    "url": "/react-client/static/media/dashboard.e78e43a1.svg"
+  },
+  {
+    "revision": "a437075a9c4bbb0d54ec978ae469fae4",
+    "url": "/react-client/static/media/feather.a437075a.svg"
+  },
+  {
+    "revision": "b1c9020bba403c7edfca40ae2075927b",
+    "url": "/react-client/static/media/home.b1c9020b.svg"
+  },
+  {
+    "revision": "8d5d3e5a9ee2201393b6a5ab17708e4a",
+    "url": "/react-client/static/media/plus.8d5d3e5a.svg"
+  }
+]);
