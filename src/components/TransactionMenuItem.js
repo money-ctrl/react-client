886e84c105984515049ce0ef2a461bf54cbae9ec
Vinícius Hoyer <vhoyer@live.com>
2019-09-22 20:35:19 -0300

    refactor: transform into function component

--- src/components/TransactionMenuItem.js
+++ src/components/TransactionMenuItem.js
@@ -1,108 +1,33 @@
 import './TransactionMenuItem.css'
 import Card from '../ui/Card'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import classNames from 'classnames'
-import { CSSTransition } from 'react-transition-group'
+import React, { useState } from 'react'
 
-class TransactionMenuItem extends React.Component {
-  constructor(props) {
-    super(props)
-    this.expand = this.expand.bind(this)
+function TransactionMenuItem({ ...props }) {
+  const [isExpanded, setExpanded] = useState(false)
 
-    this.self = React.createRef()
-
-    this.state = {
-      isExpanded:false,
-      cardStyle:{},
-    }
-  }
-
-  expand() {
-    this.setState(({isExpanded, cardStyle}) => {
-      let newCardStyle = cardStyle
-
-      if (!isExpanded) {
-        const button = ReactDOM.findDOMNode(this.self.current)
-        const bounding = button.getBoundingClientRect()
-
-        const style = {
-          closed: {
-            transitionDelay: `0s`,
-            transform: `translate(${bounding.x}px, ${bounding.y}px)`,
-            width: `${bounding.width}px`,
-            height: `${bounding.height}px`,
-          },
-          expanded: {
-            transitionDelay: `0s`,
-            transform: `translate(4px, calc(100% - 4px))`,
-            width: `calc(100vw - 8px)`,
-            height: `50vh`,
-          },
-        }
-        newCardStyle = {
-          entering: style.closed,
-          entered: style.expanded,
-          exiting: style.expanded,
-          exited: {
-            transitionDelay: `0s`,
-            height: `${bounding.height}px`,
-            width: `${bounding.width}px`,
-          },
-        }
-      }
-      //...this.state.cardStyle[state];
-
-      return {
-        isExpanded: !isExpanded,
-        cardStyle: newCardStyle,
-      }
-    });
-  }
-
-  render() {
-    const {
-      style,
-      ...attrs
-    } = this.props
-
-    const cardClasses = classNames(
-      'transaction-menu-item',
-      this.state.isExpanded && 'transaction-menu-item--is-expanded',
-    )
-
-    return (
-      <CSSTransition
-        classNames="transaction-menu-item"
-        in={this.state.isExpanded}
-        timeout={0}
+  return (
+    <Card
+      tag={isExpanded ? 'div' : 'button'}
+      onClick={() => setExpanded(!isExpanded)}
+      className="transaction-menu-item"
+      {...props}
+    >
+      <div className="transaction-menu-item__title">
+        menu item
+      </div>
+      <div
+        className="transaction-menu-item__content"
+        hidden={!isExpanded}
       >
-        <Card
-          ref={this.self}
-          tag={this.state.isExpanded ? 'div' : 'button'}
-          className={cardClasses}
-          onClick={this.expand}
-          style={style}
-          {...attrs}
-        >
-          <div className="transaction-menu-item__title">
-            menu item
-          </div>
-          <div
-            className="transaction-menu-item__content"
-            hidden={!this.state.isExpanded}
-          >
-            this is the card content!!<br />
-            this is the card content!!<br />
-            this is the card content!!<br />
-            this is the card content!!<br />
-            this is the card content!!<br />
-            this is the card content!!<br />
-          </div>
-        </Card>
-      </CSSTransition>
-    )
-  }
+        this is the card content!!<br />
+        this is the card content!!<br />
+        this is the card content!!<br />
+        this is the card content!!<br />
+        this is the card content!!<br />
+        this is the card content!!<br />
+      </div>
+    </Card>
+  )
 }
 
 export default TransactionMenuItem
