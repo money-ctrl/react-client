6b3e6d72947bbd158f7b6c682b3a9e9d3366099e
Vinícius Hoyer <vhoyer@live.com>
2020-01-19 12:43:29 -0300

    :sparkles: make card expandable

--- src/components/TransactionMenuItem/TransactionMenuItem.js
+++ src/components/TransactionMenuItem/TransactionMenuItem.js
@@ -1,21 +1,27 @@
 import './TransactionMenuItem.css'
 import Card from '../../ui/Card'
 import React, { useState } from 'react'
+import classnames from 'classnames'
 
 function TransactionMenuItem({ ...props }) {
   const [isExpanded, setExpanded] = useState(false)
+
+  const classes = classnames([
+    'transaction-menu-item',
+    isExpanded && 'transaction-menu-item--is-expanded'
+  ])
 
   return (
     <Card
       tag={isExpanded ? 'div' : 'button'}
       onClick={() => setExpanded(!isExpanded)}
-      className="transaction-menu-item"
+      className={classes}
       {...props}
     >
       <div className="transaction-menu-item__title">
         menu item
       </div>
-      <div
+      {isExpanded && <div
         className="transaction-menu-item__content"
         hidden={!isExpanded}
       >
@@ -25,7 +31,7 @@ function TransactionMenuItem({ ...props }) {
         this is the card content!!<br />
         this is the card content!!<br />
         this is the card content!!<br />
-      </div>
+      </div>}
     </Card>
   )
 }
