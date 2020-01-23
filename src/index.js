0cb5ec03e5e14ed138c52088d379d6aaf3038aa9
Vinícius Hoyer <vhoyer@live.com>
2020-01-23 08:28:40 -0300

    :recycle: import only used stuff
    remove import of hole ReactDOM in favor of just importing the render function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
+import { render } from 'react-dom'
+import React from 'react'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+render(
+  <App />,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
