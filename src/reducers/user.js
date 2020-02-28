a587af8c2c64f94ec3e5bc779e100d74463a271e
Vinícius Hoyer <vhoyer@live.com>
2020-02-28 08:39:19 -0300

    :bug: Fix login

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -6,15 +6,12 @@ const initialState = {
 }
 
 export default function(state = initialState, action) {
-  // eslint-disable-next-line no-console
-  console.log('[updade(user)]', action)
-
   switch (action.type) {
     case types.USER_LOGIN:
       return {
         ...state,
         info: action.info || {},
-        isLogged: Boolean(action.info),
+        isLogged: action.info ? !action.info.isAnonymous : false,
       }
     default:
       return state
