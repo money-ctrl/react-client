522abc61c494c7da51b4199027e5396c0fb63ae2
Vinícius Hoyer <vhoyer@live.com>
2020-03-01 11:14:20 -0300

    :recycle: Update Action button to functional syntax

--- src/ui/ActionButton/ActionButton.js
+++ src/ui/ActionButton/ActionButton.js
@@ -3,53 +3,54 @@ import React from 'react'
 import Card from '../Card'
 import classNames from 'classnames'
 import SVGPlus from '../../assets/plus.svg'
+import PropTypes from 'prop-types'
 
-class ActionButton extends React.Component {
-  constructor(props) {
-    super(props)
-    this.onClick = this.onClick.bind(this)
-    this.addAnimationDelay = this.addAnimationDelay.bind(this)
-  }
-
-  onClick() {
-    this.props.onClick()
-  }
-
-  addAnimationDelay(children) {
+function ActionButton({
+  children,
+  className,
+  isMenuOpen,
+  onClick = () => {},
+}) {
+  const addAnimationDelay = (children) => {
     return React.Children.map(children, (child, index) => React.cloneElement(child, {
       style: {animationDelay: `${(index + 1) * 0.1}s`},
     }))
   }
 
-  render() {
-    return (
-      <div className={classNames('action-button', this.props.className)}>
-        <div
-          className={classNames(this.props.isMenuOpen && 'action-button__overlay')}
-          onClick={this.onClick}
-        />
-        <div
-          aria-hidden={!this.props.isMenuOpen}
-          className={`action-button__menu ${this.props.isMenuOpen ? 'action-button__menu--is-open' : ''}`}
-        >
-          {this.addAnimationDelay(this.props.children)}
-        </div>
-        <Card
-          tag="button"
-          className="action-button__button"
-          onClick={this.onClick}
-        >
-          <img
-            className={`action-button__icon ${this.props.isMenuOpen ? 'action-button__icon--is-open' : ''}`}
-            src={SVGPlus}
-            alt="transaction"
-            height="30px"
-            width="30px"
-          />
-        </Card>
+  return (
+    <div className={classNames('action-button', className)}>
+      <div
+        className={classNames(isMenuOpen && 'action-button__overlay')}
+        onClick={onClick}
+      />
+      <div
+        aria-hidden={!isMenuOpen}
+        className={`action-button__menu ${isMenuOpen ? 'action-button__menu--is-open' : ''}`}
+      >
+        {addAnimationDelay(children)}
       </div>
-    )
-  }
+      <Card
+        tag="button"
+        className="action-button__button"
+        onClick={onClick}
+      >
+        <img
+          className={`action-button__icon ${isMenuOpen ? 'action-button__icon--is-open' : ''}`}
+          src={SVGPlus}
+          alt="transaction"
+          height="30px"
+          width="30px"
+        />
+      </Card>
+    </div>
+  )
+}
+
+ActionButton.propTypes = {
+  className: PropTypes.any,
+  children: PropTypes.any,
+  isMenuOpen: PropTypes.bool,
+  onClick: PropTypes.func,
 }
 
 export default ActionButton
