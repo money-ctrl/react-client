6d3192c06214b3bb44089de8c7264c4cfdbaa3f9
Vinícius Hoyer <vhoyer@live.com>
2020-03-03 09:35:42 -0300

    :recycle: convert CategoryCard to functional component

--- src/components/CategoriesCarousel/CategoryCard/AddCategoryCard/AddCategoryCard.js
+++ src/components/CategoriesCarousel/CategoryCard/AddCategoryCard/AddCategoryCard.js
@@ -3,19 +3,17 @@ import Card from '../../../../ui/Card'
 import MoneyDisplay from '../../../MoneyDisplay'
 import React from 'react'
 
-class CategoryCard extends React.Component {
-  render() {
-    return (
-      <Card className="add-category-card">
-        <Card className="add-category-card__progress" />
-        <MoneyDisplay
-          monochromatic={true}
-          label="Add Category"
-          value={0}
-        />
-      </Card>
-    )
-  }
+function CategoryCard() {
+  return (
+    <Card className="add-category-card">
+      <Card className="add-category-card__progress" />
+      <MoneyDisplay
+        monochromatic={true}
+        label="Add Category"
+        value={0}
+      />
+    </Card>
+  )
 }
 
 export default CategoryCard
