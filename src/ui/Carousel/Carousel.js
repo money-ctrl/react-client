0d37ee872db88d56a129033ca18e21568381605d
Vinícius Hoyer <vhoyer@live.com>
2020-03-03 11:37:09 -0300

    :recycle: Migrate carousel to functional sintax

--- src/ui/Carousel/Carousel.js
+++ src/ui/Carousel/Carousel.js
@@ -1,16 +1,25 @@
 import './Carousel.css'
 import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
-class Carousel extends React.Component {
-  render() {
-    return (
-      <div className={`carousel ${this.props.className}`}>
-        <div className="carousel__slides">
-          {this.props.children}
-        </div>
+function Carousel({ nativeRef, className, children }) {
+  return (
+    <div
+      ref={nativeRef}
+      className={classNames('carousel', className)}
+    >
+      <div className="carousel__slides">
+        {children}
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+Carousel.propTypes = {
+  nativeRef: PropTypes.any,
+  className: PropTypes.any,
+  children: PropTypes.any,
 }
 
 export default Carousel
