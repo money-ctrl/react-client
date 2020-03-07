5d9e31bd9af7623c6077d7c126d9d862b217494b
Vinícius Hoyer <vhoyer@live.com>
2020-03-07 13:42:55 -0300

    :sparkles: Add layout with a back button on top

---
+++ src/layout/TopNavigationLayout/TopNavigationLayout.js
@@ -0,0 +1,27 @@
+import './TopNavigationLayout.css'
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from '../../ui/Button'
+
+function TopNavigationLayout({ children, onBackPress }) {
+  return (
+    <div className="top-navigation-layout">
+      <div className="top-navigation-layout__toolbar">
+        <Button variant="secondary" type="round" onClick={onBackPress}>
+          <i className="fas fa-chevron-left"></i>
+        </Button>
+      </div>
+
+      <div className="top-navigation-layout__content">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+TopNavigationLayout.propTypes = {
+  children: PropTypes.any,
+  onBackPress: PropTypes.func,
+}
+
+export default TopNavigationLayout
