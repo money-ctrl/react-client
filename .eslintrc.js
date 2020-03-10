bc4908c297110457886ee7f441275210a4504079
Vinícius Hoyer <vhoyer@live.com>
2020-03-10 07:36:13 -0300

    :bug: Fix .eslintrc.js
    it had a typo on it

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
   ],
   "globals": {
     "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+    "SharedArrayBuffer": "readonly",
     "process": "readonly"
   },
   "parserOptions": {
