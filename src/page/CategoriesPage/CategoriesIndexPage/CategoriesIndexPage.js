3800ca86b976a1a564a170334a0ad5dcf9b58f34
Vinícius Hoyer <vhoyer@live.com>
2020-03-14 11:22:13 -0300

    :sparkles: Add cog to categories index page

--- src/page/CategoriesPage/CategoriesIndexPage/CategoriesIndexPage.js
+++ src/page/CategoriesPage/CategoriesIndexPage/CategoriesIndexPage.js
@@ -1,40 +1,51 @@
 import './CategoriesIndexPage.css'
 import React from 'react'
 import Title from '../../../ui/Title'
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import Loading from '../../../ui/Loading'
 import Card from '../../../ui/Card'
 import MoneyDisplay from '../../../components/MoneyDisplay'
 import Overdrive from 'react-overdrive'
+import TopNavigationLayout from '../../../layout/TopNavigationLayout'
 
 function CategoriesIndexPage() {
+  const history = useHistory()
+
   const { categoryId } = useParams()
   const category = useSelector(state => state.categories.ids[categoryId])
 
   if (!category) return <Loading/>
 
   return (<>
-    <Title title={category.name} />
+    <TopNavigationLayout
+      onBackPress={history.goBack}
+      actions={[{
+        icon: 'cog',
+        onClick: () => {alert('uhuu')},
+      }]}
+    >
+      <Title title={category.name} />
 
-    <Overdrive id={`category-card-${categoryId}`}>
-      <Card className="categories-index-page__card">
-        <MoneyDisplay
-          value={category.amount}
-        />
+      <Overdrive id={`category-card-${categoryId}`}>
+        <Card className="categories-index-page__card">
+          <MoneyDisplay
+            value={category.amount}
+          />
 
-        <div className="categories-index-page__slash">/</div>
+          <div className="categories-index-page__slash">/</div>
 
-        <MoneyDisplay
-          value={category.limit}
-          size={.5}
-        />
-      </Card>
-    </Overdrive>
+          <MoneyDisplay
+            value={category.limit}
+            size={.5}
+          />
+        </Card>
+      </Overdrive>
 
-    <div>
+      <div>
       current
-    </div>
+      </div>
+    </TopNavigationLayout>
   </>)
 }
 
--- src/page/CategoriesPage/CategoriesPage.js
+++ src/page/CategoriesPage/CategoriesPage.js
@@ -3,23 +3,17 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import CategoriesNewPage from './CategoriesNewPage'
 import CategoriesIndexPage from './CategoriesIndexPage'
-import TopNavigationLayout from '../../layout/TopNavigationLayout'
-import { useHistory } from 'react-router-dom'
 
 function CategoriesPage() {
-  const history = useHistory()
+  return (<>
+    <Route path="/categories/:categoryId">
+      <CategoriesIndexPage />
+    </Route>
 
-  return (
-    <TopNavigationLayout onBackPress={history.goBack}>
-      <Route path="/categories/:categoryId">
-        <CategoriesIndexPage />
-      </Route>
-
-      <Route path="/categories/new">
-        <CategoriesNewPage />
-      </Route>
-    </TopNavigationLayout>
-  )
+    <Route path="/categories/new">
+      <CategoriesNewPage />
+    </Route>
+  </>)
 }
 
 export default CategoriesPage
