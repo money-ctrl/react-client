29cbd280b85da02cd9e5ea473108b8a40b0609cc
Vinícius Hoyer <vhoyer@live.com>
2020-03-14 14:35:23 -0300

    :recycle: Refactor title to functional component

--- src/ui/Title/Title.js
+++ src/ui/Title/Title.js
@@ -1,14 +1,22 @@
 import './Title.css'
 import React from 'react'
+import PropTypes from 'prop-types'
 
-class Title extends React.Component {
-  render() {
-    return (
-      <h1 className="title">
-        {this.props.title}
-      </h1>
-    )
-  }
+function Title({ tag = 'h1', title }) {
+  const Heading = tag
+
+  return (
+    <Heading className="title">
+      {title}
+    </Heading>
+  )
+}
+
+Title.propTypes = {
+  title: PropTypes.string,
+  tag: PropTypes.oneOf([
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+  ]),
 }
 
 export default Title
