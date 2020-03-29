41fc6b60020a7521f110e661b2c39e48721b051f
Vinícius Hoyer <vhoyer@live.com>
2020-03-29 11:14:10 -0300

    :sparkles: [Title] support multiple color

--- src/ui/Title/Title.js
+++ src/ui/Title/Title.js
@@ -1,13 +1,20 @@
 import './Title.css'
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
-function Title({ tag = 'h1', title }) {
+function Title({ tag = 'h1', title, children, color = 'light', className }) {
   const Heading = tag
 
   return (
-    <Heading className="title">
-      {title}
+    <Heading
+      className={classNames(
+        'title',
+        color !== 'light' && `title--${color}`,
+        className,
+      )}
+    >
+      {children || title}
     </Heading>
   )
 }
@@ -15,8 +22,14 @@ function Title({ tag = 'h1', title }) {
 Title.propTypes = {
   title: PropTypes.string,
   tag: PropTypes.oneOf([
-    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'strong',
   ]),
+  children: PropTypes.any,
+  color: PropTypes.oneOf([
+    'light',
+    'dark',
+  ]),
+  className: PropTypes.string,
 }
 
 export default Title
