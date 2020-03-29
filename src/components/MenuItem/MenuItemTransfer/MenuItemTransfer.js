f29db881d4c79c599cd55aac03c77a9d40e8c712
Vinícius Hoyer <vhoyer@live.com>
2020-03-29 15:38:24 -0300

    :sparkles: Make transference work backward

--- src/components/MenuItem/MenuItemTransfer/MenuItemTransfer.js
+++ src/components/MenuItem/MenuItemTransfer/MenuItemTransfer.js
@@ -1,14 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MenuItemBase from '../MenuItemBase'
 import PropTypes from 'prop-types'
+import MoneyCalculator from '../../MoneyCalculator'
+import CategorySelector from '../../CategorySelector'
+import { addTransaction } from '../../../services/backend'
 
 function MenuItemTransfer({style}) {
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState(0)
+
+  const onTransactionSubmit = ({ amount, sender, recipient }) => {
+    addTransaction({
+      version: 2,
+      type: 'transfer',
+      amount,
+      sender: {type: 'category', ...sender},
+      recipient: {type: 'category', ...recipient},
+      transactionNature: 'transference',
+    })
+  }
+
+  const pages = [
+    ({ nextStep }) => (
+      <MoneyCalculator
+        key="amount"
+        onSubmit={(amount) => {
+          setAmount(amount)
+          nextStep()
+        }}
+      />
+    ),
+    ({ previousStep, nextStep }) => (
+      <CategorySelector
+        key="Where to take?"
+        title="Where to take?"
+        onBackPress={previousStep}
+        onSubmit={(category) => {
+          setFrom(category)
+          nextStep()
+        }}
+      />
+    ),
+    ({ previousStep, close }) => (
+      <CategorySelector
+        key="Where to deposit?"
+        title="Where to deposit?"
+        onBackPress={previousStep}
+        onSubmit={(category) => {
+          onTransactionSubmit({
+            amount,
+            sender: from,
+            recipient: category,
+          })
+          close()
+        }}
+      />
+    ),
+  ]
+
   return (
     <MenuItemBase
       style={style}
       title="Transfer"
       icon="chevron-right"
       iconColors={['#f8efff', '#9a73e4']}
+      pages={pages}
     />
   )
 }
--- src/services/backend/index.js
+++ src/services/backend/index.js
@@ -29,7 +29,88 @@ export const database = () => {
   return db.collection('users').doc(userUId)
 }
 
-export const addTransaction = async ({ income, expense }) => {
+export const addTransaction = ({ version = 1, ...options }) => {
+  if (version === 2) return addTransactionV2(options)
+
+  return addTransactionV1(options)
+}
+
+async function addTransactionV2({ amount, sender, recipient, type, transactionNature }) {
+  try {
+    validateArguments(...arguments)
+
+    const resourceIdentifier = ({type, id}) => `${type}:${id}`
+    const payload = {
+      type,
+      amount,
+      createdAt: new Date(),
+      sender: resourceIdentifier(sender),
+      recipient: resourceIdentifier(recipient),
+      relatedParties: [resourceIdentifier(sender), resourceIdentifier(recipient)],
+      displayData: {
+        sender: sender.name,
+        recipient: recipient.name,
+        transactionNature,
+      },
+    }
+
+    database().collection('transactions').doc().set(payload)
+
+    const applyTransaction = async (path, { partyType, party }) => {
+      const modifier = { 'sender': -1, 'recipient': 1 }
+      const amount = payload.amount * modifier[partyType]
+
+      const {
+        amount: previousAmount,
+      } = await path(party.id).get().then(ref => ref.data() || {})
+
+      path(party.id).set({ amount: (previousAmount||0) + amount }, {merge:true})
+    }
+
+    const collections = {
+      'category': (id) => database().collection('expenseCategories').doc(id),
+      'wallet': () => database(),
+    }
+
+    if (['wallet', 'category'].includes(sender.type)) {
+      applyTransaction(collections[sender.type], {
+        partyType: 'sender',
+        party: sender,
+      })
+    }
+
+    if (['wallet', 'category'].includes(recipient.type)) {
+      applyTransaction(collections[recipient.type], {
+        partyType: 'recipient',
+        party: recipient,
+      })
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error)
+    alert(error)
+  }
+}
+
+function validateArguments({type, amount, sender, recipient}) {
+  if (!['income', 'expense', 'transfer'].includes(type))
+    throw new Error(`Invalid transaction type: ${type}`)
+
+  if (typeof amount !== 'number')
+    throw new Error(`Invalid amount type: ${typeof amount}`)
+
+  const validateResourceIdentifierType = ({type}) => ![
+    'category',
+    'reason',
+    'wallet'
+  ].includes(type)
+  if (validateResourceIdentifierType(sender) || validateResourceIdentifierType(recipient))
+    throw new Error(`Sender with invalid type: ${sender.type}`)
+  if (validateResourceIdentifierType(recipient))
+    throw new Error(`Recipient with invalid type: ${recipient.type}`)
+}
+
+const addTransactionV1 = async ({ income, expense }) => {
   const getLastTransaction = async ({collection}) => {
     const lastTransactionQuery = await collection
       .orderBy('createdAt', 'desc')
