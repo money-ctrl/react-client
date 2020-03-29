05013494723564ae5766ff24ef249ba72d3ae9b3
Vinícius Hoyer <vhoyer@live.com>
2020-03-29 10:35:20 -0300

    :sparkles: Add title slot on top navigation layout

--- src/layout/TopNavigationLayout/TopNavigationLayout.js
+++ src/layout/TopNavigationLayout/TopNavigationLayout.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import Button from '../../ui/Button'
 import Icon from '../../ui/Icon'
 
-function TopNavigationLayout({ children, onBackPress, actions = [] }) {
+function TopNavigationLayout({ children, onBackPress, actions = [], titleSlot }) {
   return (
     <div className="top-navigation-layout">
       <div className="top-navigation-layout__toolbar">
@@ -12,7 +12,7 @@ function TopNavigationLayout({ children, onBackPress, actions = [] }) {
           <Icon name="chevron-left" />
         </Button>
 
-        {actions.map(({ icon, onClick }) => (
+        {titleSlot || actions.map(({ icon, onClick }) => (
           <Button
             key={icon}
             variant="secondary"
@@ -36,7 +36,8 @@ TopNavigationLayout.propTypes = {
   actions: PropTypes.arrayOf(PropTypes.shape({
     onClick: PropTypes.func,
     icon: PropTypes.string,
-  }))
+  })),
+  titleSlot: PropTypes.any,
 }
 
 export default TopNavigationLayout
