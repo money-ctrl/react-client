b09cb1ea3f2e0ed687a8443805f2062111214a4c
Vinícius Hoyer <vhoyer@live.com>
2020-04-12 20:42:23 -0300

    :lipstick: Change animation engine

--- src/layout/Background/Background.js
+++ src/layout/Background/Background.js
@@ -1,78 +1,47 @@
 import './Background.css'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
-function randomBetween({ min = 0, max = 1}) {
+function randomBetween({ min = 0, max = 1} = {}) {
   return Math.random() * (max - min) + min
 }
 
-function Background() {
-  const [items, setItems] = useState(Array.from({length:12}, (_value, id) => ({
-    id,
+function chooseOne(array) {
+  return array[Math.floor(randomBetween({ max: array.length }))]
+}
+
+const maxDuration = 140
+
+const items = Array.from({length:12}, (_value, index) => ({
+  id: index,
+  style: {
     width: randomBetween({ min: 80, max: 150 }),
     height: randomBetween({ min: 80, max: 150 }),
-    left: randomBetween({ max: window.innerWidth - 150 }),
-    top: randomBetween({ max: window.innerHeight - 150 }),
-    direction: Math.ceil(randomBetween({ max: 4 })),
-    velocity: Math.ceil(randomBetween({ max: 10 })),
-  })))
+    ...(chooseOne([
+      {
+        animationName: 'slide-down',
+        left: randomBetween({ max: window.innerWidth - 150 }),
+      },
+      {
+        animationName: 'slide-right',
+        top: randomBetween({ max: window.innerHeight - 150 }),
+      },
+    ])),
+    animationDirection: Math.round(randomBetween()) ? 'normal' : 'reverse',
+    animationDuration: `${randomBetween({ min: maxDuration -25, max: maxDuration })}s`,
+    animationDelay: `${randomBetween({ min: -maxDuration, max: 0 })}s`,
+    animationTimingFunction: 'linear',
+    animationIterationCount: 'infinite',
+  },
+}))
 
-  const interval = 10
-
-  const moveItem = (item) => {
-    const {left, top} = item
-
-    const newPosition = { left, top }
-    const velocity = item.velocity * (interval/1000)
-
-    switch(item.direction) {
-      case 1://left
-        newPosition.left += velocity
-        break
-      case 2://right
-        newPosition.left -= velocity
-        break
-      case 3://top
-        newPosition.top += velocity
-        break
-      default://bottom
-        newPosition.top -= velocity
-        break
-    }
-
-    if (newPosition.left + item.width < 0) newPosition.left = window.innerWidth
-    if (newPosition.left > window.innerWidth) newPosition.left = -item.width
-    if (newPosition.top + item.height < 0) newPosition.top = window.innerHeight
-    if (newPosition.top > window.innerHeight) newPosition.top = -item.height
-
-    return {
-      ...item,
-      ...newPosition,
-    }
-  }
-
-  useEffect(() => {
-    let animationHandler
-    let requestId = requestAnimationFrame(animationHandler = () => {
-      setItems(items => items.map(moveItem))
-      requestId = requestAnimationFrame(animationHandler)
-    })
-
-    return () => {
-      window.cancelAnimationFrame(requestId)
-    }
-  }, [])
-
+function Background() {
   return (
     <div className="background">
-      {items.map(({top, left, height, width, id}) =>
+      {items.map(({style, id}) =>
         <div
           key={id}
           className="background__item"
-          style={{
-            transform: `translate(${left}px, ${top}px)`,
-            height,
-            width,
-          }}
+          style={style}
         />)
       }
     </div>
