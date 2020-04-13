b8b9e0b74da95d8e91b021c376b4ff445e6f2a1d
Vinícius Hoyer <vhoyer@live.com>
2020-04-12 21:13:20 -0300

    :zap: Try improving animation performance by reducing moving parts

--- src/components/MoneyCalculator/MoneyCalculator.js
+++ src/components/MoneyCalculator/MoneyCalculator.js
@@ -6,8 +6,6 @@ import PropTypes from 'prop-types'
 import Icon from '../../ui/Icon'
 
 function MoneyCalculator({ onSubmit = (() => {}) }) {
-  const buttons = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
-
   const [value, setValue] = useState(0)
 
   return (
@@ -20,38 +18,65 @@ function MoneyCalculator({ onSubmit = (() => {}) }) {
           maximumFractionDigits: 2,
         }}
       />
-      {buttons.map((number) =>
+      {[
+        [7, 8, 9],
+        [4, 5, 6],
+        [1, 2, 3],
+      ].map((numbers, index) => (
         <div
-          key={number}
-          onClick={() => setValue(value * 10 + number)}
+          key={index}
+          className="money-calculator__row"
+        >
+          {numbers.map((number) => (
+            <div
+              key={number}
+              onClick={() => setValue(value * 10 + number)}
+              className="money-calculator__button-wrapper"
+            >
+              <Button
+                type="round"
+                variant="ghost"
+                className="money-calculator__number"
+              >
+                {number}
+              </Button>
+            </div>
+          ))}
+        </div>
+      ))}
+      <div className="money-calculator__row">
+        <div className="money-calculator__back money-calculator__button-wrapper">
+          <Button
+            type="round"
+            variant="ghost"
+            onClick={() => setValue(Math.floor(value / 10))}
+          >
+            <Icon name="backspace" />
+          </Button>
+        </div>
+
+        <div
+          onClick={() => setValue(value * 10)}
+          className="money-calculator__button-wrapper"
         >
           <Button
             type="round"
             variant="ghost"
             className="money-calculator__number"
           >
-            {number}
+            0
           </Button>
         </div>
-      )}
-      <div className="money-calculator__enter">
-        <Button
-          type="round"
-          variant="primary"
-          onClick={() => onSubmit(value/100)}
-        >
-          <Icon name="check" />
-        </Button>
-      </div>
 
-      <div className="money-calculator__back">
-        <Button
-          type="round"
-          variant="ghost"
-          onClick={() => setValue(Math.floor(value / 10))}
-        >
-          <Icon name="backspace" />
-        </Button>
+        <div className="money-calculator__enter money-calculator__button-wrapper">
+          <Button
+            type="round"
+            variant="primary"
+            onClick={() => onSubmit(value/100)}
+          >
+            <Icon name="check" />
+          </Button>
+        </div>
       </div>
     </div>
   )
