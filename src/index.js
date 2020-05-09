bda74de02074f67e0173ad319080e4a4007f4c2f
Vinícius Hoyer <vhoyer@live.com>
2020-05-09 20:03:53 -0300

    :children_crossing: Remove outline if you don't really need it :D

--- src/index.js
+++ src/index.js
@@ -17,6 +17,12 @@ render(
   document.getElementById('root')
 )
 
+window.addEventListener('keyup', (e) => {
+  if (e.key !== 'Tab') { return }
+
+  document.documentElement.classList.remove('no-focus-outline')
+})
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
