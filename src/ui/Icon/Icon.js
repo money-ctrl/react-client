e5d8dbef624d9c2f90521ae92e7a67916f461fdd
Vinícius Hoyer <vhoyer@live.com>
2020-05-10 17:02:04 -0300

    :sparkles: Give more versatility to Icon

--- src/ui/Icon/Icon.js
+++ src/ui/Icon/Icon.js
@@ -1,6 +1,7 @@
 import './Icon.css'
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 const ICONS = Object.freeze({
   'plus': 'fas fa-plus',
@@ -8,14 +9,36 @@ const ICONS = Object.freeze({
   'chevron-right': 'fas fa-chevron-right',
 })
 
-function Icon({name}) {
+const fontModifiers = {
+  'xs': 0.25,
+  's': 0.5,
+  'm': 1,
+  'l': 1.5,
+  'xl': 2,
+  'xxl': 2.5,
+}
+
+function Icon({className, style = {}, name, size = 'm'}) {
+  const fontModifier = fontModifiers[size]
+
+  const classes = classNames(
+    ICONS[name] || `fas fa-${name}`,
+    className,
+  )
+
   return (
-    <i className={ICONS[name] || `fas fa-${name}`} />
+    <i
+      className={classes}
+      style={{ ...style, fontSize: `${fontModifier}rem` }}
+    />
   )
 }
 
 Icon.propTypes = {
+  className: PropTypes.string,
   name: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(Object.keys(fontModifiers)),
+  style: PropTypes.object,
 }
 
 export default Icon
