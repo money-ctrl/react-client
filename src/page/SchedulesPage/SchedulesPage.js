f954e489472350d05b29c3b30041b561d4ac2622
Vinícius Hoyer <contact@vhoyer.dev>
2021-04-26 09:31:25 -0300

    :sparkles: Add back button to schedules page

--- src/page/SchedulesPage/SchedulesPage.js
+++ src/page/SchedulesPage/SchedulesPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import TopNavigationLayout from '../../layout/TopNavigationLayout'
 import Title from '../../ui/Title'
+import { useHistory } from 'react-router-dom'
 
 function SchedulesPage() {
-  return <TopNavigationLayout>
+  const history = useHistory()
+
+  return <TopNavigationLayout onBackPress={history.goBack}>
     <Title tag="h1" title="Schedules" />
 
     hey, that&apos;s when I began to pray
