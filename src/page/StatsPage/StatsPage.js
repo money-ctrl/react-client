ea2670dce751ccfd679aa4962aad404c90d3602a
Vinícius Hoyer <contact@vhoyer.dev>
2021-05-20 21:47:34 -0300

    :sparkles: Add more info on stat page

--- src/page/StatsPage/StatsPage.js
+++ src/page/StatsPage/StatsPage.js
@@ -4,12 +4,14 @@ import { useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
 import MoneyDisplay from '../../components/MoneyDisplay'
 import Title from '../../ui/Title'
+import Divider from '../../ui/Divider'
 
 function StatsPage() {
   const categories = useSelector(state => state.categories.list)
 
   const amountSum = categories.reduce((acc, cur) => acc + cur.amount, 0)
   const allocatedSum = categories.reduce((acc, cur) => acc + cur.allocated, 0)
+  const debtSum = categories.reduce((acc, cur) => acc + cur.debt, 0)
 
   const totalMoney = useSelector(state => state.money.amount)
 
@@ -25,11 +27,27 @@ function StatsPage() {
         monochromatic={true}
         value={totalMoney}
       />
+
+      <MoneyDisplay
+        label="Total debt in categories"
+        monochromatic={true}
+        value={debtSum}
+      />
+
+      <MoneyDisplay
+        label="Balance accounting debt"
+        monochromatic={true}
+        value={totalMoney + debtSum}
+      />
+
+      <Divider />
+
       <MoneyDisplay
         label="Cycle's Total Budget"
         monochromatic={true}
         value={allocatedSum}
       />
+
       <MoneyDisplay
         label="Cycle's Current Balance"
         monochromatic={true}
