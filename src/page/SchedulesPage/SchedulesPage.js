21051ba71cc34eaf36e14d6abe9def968f614110
Vinícius Hoyer <contact@vhoyer.dev>
2022-02-28 18:15:23 -0300

    :sparkles: [SchedulesPage] make presentation better

--- src/page/SchedulesPage/SchedulesPage.js
+++ src/page/SchedulesPage/SchedulesPage.js
@@ -1,11 +1,11 @@
 import './SchedulesPage.css'
-import React, { Fragment } from 'react'
+import React from 'react'
 import TopNavigationLayout from '../../layout/TopNavigationLayout'
 import Title from '../../ui/Title'
 import { useHistory } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import Card from '../../ui/Card'
-import { flat } from '../../utils'
+import MoneyDisplay from '../../components/MoneyDisplay'
 
 function SchedulesPage() {
   const history = useHistory()
@@ -17,12 +17,29 @@ function SchedulesPage() {
     {scheduleList.map(schedule => (
       <Card key={schedule.id} className="p-m mb-s w-100">
         <dl>
-          {Object.entries(flat(schedule)).map(([key, value]) => (
-            <Fragment key={key}>
-              <dt className="schedules-page__list-title">{key}</dt>
-              <dd className="schedules-page__list-description">{JSON.stringify(value)}</dd>
-            </Fragment>
-          ))}
+          <dt className="schedules-page__list-title">Nature:</dt>
+          <dd className="schedules-page__list-description">
+            {JSON.stringify(schedule.transactionPayload.transactionNature)}
+          </dd>
+
+          <dt className="schedules-page__list-title">Amount:</dt>
+          <dd className="schedules-page__list-description">
+            <MoneyDisplay value={schedule.transactionPayload.amount} size="xxs" />
+          </dd>
+
+          <dt className="schedules-page__list-title">On category:</dt>
+          <dd className="schedules-page__list-description">
+            {JSON.stringify(schedule.transactionPayload.sender.name)}
+          </dd>
+
+          <dt className="schedules-page__list-title">repeatCount:</dt>
+          <dd className="schedules-page__list-description">{JSON.stringify(schedule.repeatCount)}</dd>
+
+          <dt className="schedules-page__list-title">triggerType:</dt>
+          <dd className="schedules-page__list-description">{JSON.stringify(schedule.triggerType)}</dd>
+
+          <dt className="schedules-page__list-title">raw:</dt>
+          <dd className="schedules-page__list-description">{JSON.stringify(schedule)}</dd>
         </dl>
       </Card>
     ))}
