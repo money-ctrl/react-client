b6811ce7f967315ac9002c4848982b2c2c7f973d
Vinícius Hoyer <contact@vhoyer.dev>
2022-05-08 11:05:05 -0300

    :recycle: Refactor the expense menu item

--- src/components/MenuItem/MenuItemExpense/MenuItemExpense.js
+++ src/components/MenuItem/MenuItemExpense/MenuItemExpense.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MenuItemBase from '../MenuItemBase'
 import PropTypes from 'prop-types'
 import { addTransaction } from '../../../services/backend'
@@ -7,54 +7,57 @@ import { useSelector } from 'react-redux'
 import { useRouteMatch } from 'react-router-dom'
 import CategorySelector from '../../CategorySelector'
 
+const Execute = ({ onRender }) => {
+  useEffect(() => onRender(), [onRender])
+  return null
+}
+
 function MenuItemExpense({style, onSubmit}) {
-  const onExpenseSubmit = (sender, close, value = 0) => {
-    addTransaction({
-      type: 'expense',
-      transactionNature: window.prompt() || 'Nature not specified',
-      amount: value,
-      sender: {type: 'category', ...sender},
-      recipient: {
-        type: 'reason',
-        name: 'Unknown',
-        id: 'unknown',
-      },
-    })
+  const categoriesPageMatch = useRouteMatch('/categories/:categoryId')
+  const categories = useSelector(state => state.categories.ids)
 
-    close()
-    onSubmit()
-  }
-
-  let pages = [
-    ({ nextStep }) => (
-      <MoneyCalculator
-        onSubmit={(amount) => {
-          nextStep({ amount })
-        }}
-      />
-    ),
-    ({ previousStep, close, payload: { amount } }) => (
+  const pages = [
+    ({ previousStep, nextStep }) => (
       <CategorySelector
         onBackPress={previousStep}
-        onSubmit={category => onExpenseSubmit(category, close, amount)}
+        onSubmit={category => nextStep({ category })}
         title="Where to spend?"
       />
     ),
+    ({ previousStep, nextStep }) => (
+      <MoneyCalculator
+        onBackPress={previousStep}
+        onSubmit={(amount) => nextStep({ amount })}
+      />
+    ),
+    ({ close, payload }) => (
+      <Execute onRender={() => {
+        const { category, amount } = payload
+
+        addTransaction({
+          type: 'expense',
+          transactionNature: window.prompt() || 'Nature not specified',
+          amount,
+          sender: { type: 'category', ...category },
+          recipient: { type: 'reason', name: 'Unknown', id: 'unknown' },
+        })
+
+        close()
+        onSubmit()
+      }} />
+    ),
   ]
 
-  const categoriesPageMatch = useRouteMatch('/categories/:categoryId')
-  const categories = useSelector(state => state.categories.ids)
   if (categoriesPageMatch) {
     const { categoryId } = categoriesPageMatch.params
     const categoryFromUrl = categories[categoryId]
 
-    pages = [({ close }) => (
-      <MoneyCalculator
-        onSubmit={(amount) => {
-          onExpenseSubmit(categoryFromUrl, close, amount)
-        }}
-      />
-    )]
+    pages.shift()
+    pages.unshift(({ nextStep }) => (
+      <Execute onRender={() => nextStep({
+        category: categoryFromUrl,
+      })} />
+    ))
   }
 
   return (
