64bb8d39d6156cccb17794d28062757e2c391b8f
Vinícius Hoyer <contact@vhoyer.dev>
2022-05-08 12:22:44 -0300

    :sparkles: Add ui/Label component

---
+++ src/ui/Label/Label.js
@@ -0,0 +1,38 @@
+import './Label.css'
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+function Label({ className, children, name, label, ...rest }) {
+  return (
+    <div
+      className={classnames([
+        'label',
+        className,
+      ])}
+    >
+      <label
+        htmlFor={name}
+        className="label__label"
+        {...rest}
+      >
+        {label}
+      </label>
+
+      {children({
+        id: name,
+        name,
+        placeholder: label,
+      })}
+    </div>
+  )
+}
+
+Label.propTypes = {
+  children: PropTypes.node,
+  label: PropTypes.string,
+  name: PropTypes.string,
+  className: PropTypes.any,
+}
+
+export default Label
