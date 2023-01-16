f23a23c59819318c96efc1e438141342fe2d8c98
Vinícius Hoyer <contact@vhoyer.dev>
2023-01-16 11:32:53 -0300

    :wrench: [vite] add @/ alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
 export default defineConfig({
   root: './src',
+  resolve: {
+    alias: [
+      {
+        find: /^@\//,
+        replacement: path.resolve(__dirname, './src/') + '/',
+      }
+    ],
+  },
   server: {
     port: Number(process.env.PORT),
   },
